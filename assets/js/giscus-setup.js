79a12d6ea9c0da84e3e8f3d67cf7a153ce00a0a3
AhmedIbrahimElsharkawy <34901331+AhmedIbrahimElsharkawy@users.noreply.github.com>
2026-01-25 09:20:11 +0000

    Deploying to gh-pages from @ AhmedIbrahimElsharkawy/AhmedIbrahimElsharkawy.github.io@70a1f65ae49a8d42dcdc42735d76016316d0df8f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AhmedIbrahimElsharkawy/Ahmed_Elsharkawy",
+    "data-repo": "AhmedIbrahimElsharkawy/AhmedIbrahimElsharkawy.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
